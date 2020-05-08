5ac14cc23511a7f8de48f55e1e8070a21c16ff6a
tangoboy <137757831@qq.com>
2020-05-08 19:11:07 +0800

    transform-runtime

--- apis.out.js
+++ apis.out.js
@@ -1,17 +1,17 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
 
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
 var _mpCloudSdk = _interopRequireDefault(require("@tbmp/mp-cloud-sdk"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
-
-function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 var _default = function _default(config) {
   _mpCloudSdk["default"].init({
@@ -180,9 +180,9 @@ var _default = function _default(config) {
     },
     icss: function icss() {
       var _arguments = arguments;
-      return _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+      return (0, _asyncToGenerator2["default"])(_regenerator["default"].mark(function _callee() {
         var page;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
+        return _regenerator["default"].wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
@@ -346,9 +346,9 @@ var _default = function _default(config) {
     },
     tobeMember: function tobeMember(page) {
       var _arguments2 = arguments;
-      return _asyncToGenerator(regeneratorRuntime.mark(function _callee2() {
+      return (0, _asyncToGenerator2["default"])(_regenerator["default"].mark(function _callee2() {
         var params, memberUrl, backUrl;
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+        return _regenerator["default"].wrap(function _callee2$(_context2) {
           while (1) {
             switch (_context2.prev = _context2.next) {
               case 0:
@@ -392,9 +392,9 @@ var _default = function _default(config) {
     },
     cloudFile: function cloudFile() {
       var _arguments3 = arguments;
-      return _asyncToGenerator(regeneratorRuntime.mark(function _callee3() {
+      return (0, _asyncToGenerator2["default"])(_regenerator["default"].mark(function _callee3() {
         var urls, map, resUrls;
-        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+        return _regenerator["default"].wrap(function _callee3$(_context3) {
           while (1) {
             switch (_context3.prev = _context3.next) {
               case 0:
